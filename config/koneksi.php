a75a8ec86b9210d3ac4ac7c39c641eeb68dee2bb
Kevin Martinda Sodikin <43369306+kevinmartinda@users.noreply.github.com>
2018-09-20 21:25:37 +0700

    Update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,8 +1,8 @@
 <?php 
-	$SERV = 'localhost';
-	$USER = 'root';
-	$PASS = '';
-	$DBNM = 'db_absensi';
+	$SERV = 'your serv';
+	$USER = 'your user';
+	$PASS = 'your pass';
+	$DBNM = 'yourdbnm';
 
 		$conn = mysqli_connect($SERV, $USER, $PASS, $DBNM) or die();
  ?>
